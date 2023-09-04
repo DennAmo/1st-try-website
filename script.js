f400d8f829ee214a95559c23bc3ccb033f0dea20
DennAmo <dennisaubryy@gmail.com>
2023-09-04 09:04:34 +0200

    finish desktop miss star

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let bouton = document.getElementById("button1");
+
+    if (bouton) {
+        bouton.style.position = "relative";
+    }
+});
