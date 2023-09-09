50ae21cffb8e20fd8a36085b8302f69b932e9a39
DennAmo <dennisaubryy@gmail.com>
2023-09-09 13:42:46 +0200

    mobile+largedesktop miss

--- diary/diary.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\Bruuuh\Downloads\diary\
-py diary.py
--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let bouton = document.getElementById("button1");
-
-    if (bouton) {
-        bouton.style.position = "relative";
-    }
-});
